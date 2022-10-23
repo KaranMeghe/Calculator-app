60eb137923724457ae37b0f970d1d823f2b46f61
Karan Meghe <karanmeghe87@gmail.com>
2022-10-23 14:56:54 +0530

    Event listner set for buttons and functionality for AC button update

---
+++ script.js
@@ -0,0 +1,28 @@
+const calculatorDisplay = document.querySelector("h1");
+const inputBtns = document.querySelectorAll("button");
+const clearBtn = document.getElementById("clear-btn");
+
+function sendNumberValue(number){
+  // if current display value is 0 then replace it with number,else addNumber
+  const displayNumber = calculatorDisplay.textContent;
+  calculatorDisplay.textContent = displayNumber === '0' ? number : displayNumber + number;
+}
+
+// Add eventListner for num,op,dec buttons
+inputBtns.forEach((inputBtn) =>{
+  if(inputBtn.classList.length === 0){
+    inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+  }else if(inputBtn.classList.contains('operator')){
+    inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+  }else if(inputBtn.classList.contains('decimal')){
+    inputBtn.addEventListener('click', () => addDecimal())
+  }
+});
+
+// Reset Display
+function resetDisplay(){
+  calculatorDisplay.textContent = '0';
+}
+
+clearBtn.addEventListener('click',resetDisplay);
+ 
