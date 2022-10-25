eb147f8a33b14fd11f7cb409b51825364a3b94de
Karan Meghe <karanmeghe87@gmail.com>
2022-10-25 17:43:13 +0530

    calculation functionality adds

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const inputBtns = document.querySelectorAll("button");
 const clearBtn = document.getElementById("clear-btn");
 
 let firstValue = 0;
-let operatorValue = 0;
+let operatorValue = '';
 let awatingNextValue = false;
 
 function sendNumberValue(number){
@@ -12,35 +12,56 @@ function sendNumberValue(number){
     calculatorDisplay.textContent = number;
     awatingNextValue = false;
   } else {
-    // if current display value is 0 then replace it with number,else addNumber
+  // if current display value is 0, then replace it, with number,else addNumber
   const displayNumber = calculatorDisplay.textContent;
   calculatorDisplay.textContent = displayNumber === '0' ? number : displayNumber + number;
 }
 }
 
 function addDecimal(){
-  // if operator is pressed add no decimal
-  if(awatingNextValue) return;
+  // if operator is pressed, dont add decimal
+  if(awatingNextValue === true) return;
   // if no decimal add one
   if(!calculatorDisplay.textContent.includes('.')){
     calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
   } 
 }
 
+// Calculate first and second value depend on operator
+const calculate = {
+ '/' : (firstNumber, secondNumber) => firstNumber / secondNumber,
+
+ '*' : (firstNumber, secondNumber) => firstNumber * secondNumber,
+
+ '+' : (firstNumber, secondNumber) => firstNumber + secondNumber,
+
+ '-' : (firstNumber, secondNumber) => firstNumber - secondNumber,
+ 
+ '=' : (firstNumber, secondNumber) => secondNumber,
+}
+
 function userOperator(operator){
    const currentValue = Number(calculatorDisplay.textContent);
+  // Prevent multipule operators
+  if(operatorValue && awatingNextValue) {
+    operatorValue = operator;
+    return;
+  }
   // Assign first value to current value only if first value dosent exist
   if(!firstValue){
     firstValue = currentValue;
   } else{
-    console.log('cv', currentValue);
-  }
-  // Ready for next value, store our operator
+    // console.log(firstValue, operatorValue, currentValue);
+    const calculation = calculate[operatorValue](firstValue,currentValue);
+    firstValue = calculation;
+    calculatorDisplay.textContent = calculation;
+  //   console.log('calculation', calculation);
+   }
+  // Ready for next value, and store our operator
   awatingNextValue = true;
   operatorValue = operator;
-  console.log('opv', operatorValue);
-  console.log('fv',firstValue);
 }
+
 
 // Add eventListner for num,op,dec buttons
 inputBtns.forEach((inputBtn) =>{
@@ -53,9 +74,9 @@ inputBtns.forEach((inputBtn) =>{
   }
 });
 
-// Reset all values Display
+// Reset all values, Display
 function resetDisplay(){
-  firstValue = 0;
+  firstValue = 0; 
   operatorValue = "";
   awatingNextValue = false;
   calculatorDisplay.textContent = '0';
